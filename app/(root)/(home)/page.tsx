b9fc63a811e963b1fd0fff76e0edf19a3f9d3275
Jakhon <example@gmail.com>
2024-08-06 14:57:05 +0500

    Footer

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import MaxWidthWrapper from '@/components/shared/max-width-wrapper'
 import Phone from '@/components/shared/phone'
-import { Check, Star } from 'lucide-react'
+import { ArrowRight, Check, Star } from 'lucide-react'
 import ValueProposition from '../_components/value'
+import { Icons } from '@/components/shared/icons'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
 
 export default function HomePage() {
 	return (
@@ -101,7 +105,70 @@ export default function HomePage() {
 			</MaxWidthWrapper>
 
 			<ValueProposition />
+			<section>
+				<MaxWidthWrapper className='py-24'>
+					<div className='mb-12 px-16 lg:px-8'>
+						<div className='mx-auto max-w-2xl sm:text-center'>
+							<h2 className='mg:text-6xl order-1  mt-2 text-balance text-center text-5xl font-bold !leading-tight tracking-tight text-gray-600'>
+								Upload your photo and get
+								<span className='relative px-2'>
+									your own case
+									<Icons.underline className='pointer-events-none absolute inset-x-0  -bottom-6 hidden text-green-600 sm:block' />{' '}
+								</span>{' '}
+								now
+							</h2>
+						</div>
+					</div>
+					<div className='mx-auto max-w-6xl px-6 lg:px-8'>
+						<div className='relative flex grid-cols-2 flex-col items-center gap-40 md:grid'>
+							<img
+								src='/arrow.png'
+								className='absolute left-1/2 top-[25rem] z-10 -translate-x-1/2 -translate-y-1/2 rotate-90 md:top-1/2 md:rotate-0'
+							/>
+
+							<div className='relative h-80 w-full max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 md:h-full md:justify-self-end lg:rounded-2xl'>
+								<img
+									src='/horse.jpg'
+									className='size-full rounded-md bg-white object-cover shadow-2xl ring-1 ring-gray-900/10'
+								/>
+							</div>
+
+							<Phone className='w-60' imgSrc='/horse_phone.jpg' />
+						</div>
+					</div>
+
+					<ul className='mx-auto mt-12 w-fit max-w-prose space-y-2 sm:text-lg'>
+						<li className='w-fit'>
+							<Check className='mr-1.5 inline size-5 text-green-600' />
+							High-quality silicone material
+						</li>
+						<li className='w-fit'>
+							<Check className='mr-1.5 inline size-5 text-green-600' />
+							Scratch- and fingerprint resistant coating
+						</li>
+						<li className='w-fit'>
+							<Check className='mr-1.5 inline size-5 text-green-600' />
+							Wireless charging compatible
+						</li>
+						<li className='w-fit'>
+							<Check className='mr-1.5 inline size-5 text-green-600' />5 year
+							print warranty
+						</li>
+
+						<div className='flex justify-center'>
+							<Link
+								className={buttonVariants({
+									size: 'lg',
+									className: 'mx-auto mt-8',
+								})}
+								href='/configure/upload'
+							>
+								Create your case now <ArrowRight className='ml-1.5 size-4' />
+							</Link>
+						</div>
+					</ul>
+				</MaxWidthWrapper>
+			</section>
 		</div>
-		// {value proposition section}
 	)
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/shared/footer'
 import Navbar from '@/components/shared/navbar'
 import { ChildProps } from '@/types'
 import React from 'react'
@@ -7,6 +8,7 @@ function layout({ children }: ChildProps) {
 		<main>
 			<Navbar />
 			{children}
+			<Footer />
 		</main>
 	)
 }
