a691fc98b097ce75adc87177b6a66b0e50e35ed6
Jakhon <example@gmail.com>
2024-08-24 10:56:44 +0900

    Dark mode

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/shared/footer'
 import Navbar from '@/components/shared/navbar'
 import { ChildProps } from '@/types'
 import React from 'react'
+import NextTopLoader from 'nextjs-toploader';
 
 function layout({ children }: ChildProps) {
 	return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { ChildProps } from '@/types'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { Toaster } from 'sonner'
-
+import NextTopLoader from 'nextjs-toploader'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -23,6 +23,21 @@ export default function RootLayout({ children }: ChildProps) {
 					enableSystem
 					disableTransitionOnChange
 				>
+					<NextTopLoader
+						color='#2299DD'
+						initialPosition={0.08}
+						crawlSpeed={200}
+						height={3}
+						crawl={true}
+						showSpinner={false}
+						easing='ease'
+						speed={200}
+						// shadow='0 0 10px #2299DD,0 0 5px #2299DD'
+						template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+						zIndex={1600}
+						showAtBottom={false}
+					/>
 					{children}
 					<Toaster position='top-center' />
 				</ThemeProvider>
